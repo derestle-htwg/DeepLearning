648efac49af885644f6240808e26be028aa96b89
derestle-htwg <derestle@htwg-konstanz.de>
2017-04-28 00:17:46 +0200

    A2 läuft jetzt.

--- neuronalNetwork_2.py
+++ neuronalNetwork_2.py
@@ -1,196 +1,123 @@
-# -*- coding: utf-8 -*-
+from unittest.test.testmock.testpatch import something
+
 import numpy as np
+from sklearn import neural_network
+alpha = -0.01
 
-alpha = 1
 
-def gradA(lNext, index):
+def gradSig(x):
+    tmp = sig(x)
+    return 1 - (np.multiply(tmp, tmp))
+    # for x in np.nditer(L, op_flags=['readwrite']):
+    # return np.multiply(sig(x), (1 - sig(x)))
 
-    w_j1 = np.zeros([1, len(lNext.neurons)])
-    gradA_j1 = np.zeros([1, len(lNext.neurons)])
-    l_j1 = np.zeros([1, len(lNext.neurons)])
-    for i in range(len(lNext.neurons)):
-        tmp = lNext.neurons[i].W[0, index]
-        w_j1[0, i] = tmp
-        tmp = lNext.neurons[i].dA
-        gradA_j1[0, i] = tmp
-        tmp = lNext.neurons[i].L
-        l_j1[0, i] = tmp
 
-    tmp = np.dot(np.dot(np.matrix(gradSig(l_j1)), np.matrix(gradA_j1)), np.matrix(w_j1.T))
-    return tmp
-#np.dot(np.dot(np.matrix(w_j1), np.matrix(gradA_j1)), np.matrix(gradSig(l_j1.T)))
+def gradA(w_j1, grada_j1, l_j1):
+    return np.dot(np.dot(w_j1, grada_j1), gradSig(l_j1))
 
-def gradB(deltA_j, L_j):
-    return np.dot(deltA_j, gradSig(L_j))
+
+def gradB(deltA_j, l_j):
+    return np.dot(deltA_j, gradSig(l_j))
+
 
 def gradW(deltA_j, L_j, A_jm1):
-    return np.dot(np.dot(deltA_j, gradSig(L_j)).T, A_jm1.T)
-    #return np.dot(gradB(deltA_j, L_j), A_jm1)
+    return np.dot(gradB(deltA_j, L_j), A_jm1)
+
 
 def sig(t):
-        return 1.0/(1.0 + np.exp(t))
+        #return 1.0/(1.0 + np.exp(t))
+    return np.tanh(t)
 
-def gradSig(t):
-        return np.multiply(sig(t), (1-sig(t)))
 
 class Layer:
     def __init__(self, neuronCount, prevLayer):
-        self.neurons = []*neuronCount
-        self.A = np.zeros((1,neuronCount))
-        self.L = np.zeros((1,neuronCount))
+        self.dA = np.zeros((neuronCount, 1))
+        self.dB = np.zeros((neuronCount, 1))
+        self.dW = np.zeros((neuronCount, 1))
+        self.A = np.zeros((neuronCount, 1))
+        self.L = np.zeros((neuronCount, 1))
+        self.B = np.random.rand(neuronCount, 1)
+        self.W = np.zeros((0, 0))
         self.prevLayer = prevLayer
         self.nextLayer = 0
         if self.prevLayer != 0:
+            self.W = np.random.rand(neuronCount, prevLayer.A.shape[0])
             self.prevLayer.nextLayer = self
-        for i in range(neuronCount):
-            self.neurons.append(Neuron(prevLayer))
-
 
     def forward(self):
-        for i in range(len(self.neurons)):
-            self.neurons[i].calc()
-            self.A[0, i] = self.neurons[i].A
-            self.L[0, i] = self.neurons[i].L
+        if self.prevLayer != 0:
+            self.L = self.W.dot(self.prevLayer.A) + self.B
+            self.A = sig(self.L)
 
-
-class Neuron:
-    def __init__(self, prevLayer):
-        self.prevLayer = prevLayer
-        self.dB = 0.0
-        self.L = 0.0
-        self.A = 0.0
-        self.B = 0.0
-        self.dW = np.zeros((1, 1))
-        self.W = np.zeros((1, 1))
-        self.dA = 0
-        if prevLayer != 0:
-            self.dW = np.zeros(prevLayer.A.shape)
-            self.W = np.random.rand(prevLayer.A.shape[0], prevLayer.A.shape[1])
-
-    def calc(self):
-        if self.prevLayer == 0:
-            return
-
-        # Gewichtete Summe d. vorherigen neuronen berechnen
-        tmp = 0.0
-        for i in range(np.shape(self.W)[0]):
-            tmp += self.W[0,i] * self.prevLayer.A[0,i]
-        self.L = tmp + self.B
-        self.A = sig(self.L)
-
-    def setA(self, value):
-        self.A = value
 
 In = Layer(2, 0)
 Mid = Layer(2, In)
 Out = Layer(3, Mid)
+Layers = [In, Mid, Out]
 
-#Mid.neurons[0].W[0, 0] = (10)
-#Mid.neurons[0].W[0, 1] = (10)
-#Mid.neurons[0].B = -15
-#Mid.neurons[1].W[0,0] = (10)
-#Mid.neurons[1].W[0,1] = (10)
-#Mid.neurons[1].B = -5
-#
-#Out.neurons[0].W[0,0] = (10)
-#Out.neurons[0].W[0,1] = (0)
-#Out.neurons[0].B = -4
-#
-#Out.neurons[1].W[0,0] = (0)
-#Out.neurons[1].W[0,1] = (10)
-#Out.neurons[1].B = -4
-#
-#Out.neurons[2].W[0,0] = (-20)
-#Out.neurons[2].W[0,1] = (10)
-#Out.neurons[2].B = -4
+def backPropNN(E, Layers, Y):
 
-def backPropNN(E,Layers,Y):
+    dA = Layers[-1].A - Y
+    Layers[-1].dB = np.multiply(dA, gradSig(Layers[-1].L))
+    Layers[-1].dW = np.dot(Layers[-1].dB, Layers[-2].A.T)
 
-    for i in range(len(Layers[-1].neurons)):
-        Layers[-1].neurons[i].dA = Y[i] - Layers[-1].A[0,i]
-        Layers[-1].neurons[i].dB =  gradB(np.matrix(Layers[-1].neurons[i].dA), np.matrix(Layers[-1].L[0, i]))
-                                    #dA*gradSig(Layers[-1].L[0,i])
-        Layers[-1].neurons[i].dW = gradW(np.matrix(Layers[-1].neurons[i].dA),
-                                         np.matrix(gradSig(Layers[-1].L[0, i])),
-                                         np.matrix(Layers[-2].A))
-                                        #np.dot(np.matrix(Layers[-1].neurons[i].dB),np.matrix(Layers[-2].A))
-    print("backProp")
     for l in reversed(Layers[1:-1]):
-        print("NewLayer")
-        for i in range(len(l.neurons)):
-            print("neuron ", i)
-            n = l.neurons[i]
-            n.dA = gradA(l.nextLayer, i)
-            print(n.dA)
-            n.dB = gradB(n.dA, n.L)
-            print(n.dB)
-            n.dW = gradW(n.dA, n.L, np.matrix(l.A))
-            print(n.dW)
+        l.dA = l.nextLayer.W.T.dot(l.nextLayer.dB)
+        l.dB = np.multiply(l.dA, gradSig(l.L))
+        l.dW = l.dB.dot(l.prevLayer.A.T)
 
-
+    i = 0
     for l in Layers[1:]:
-        for n in l.neurons:
+        i = i + 1
+        # print(i)
+        # print(l.dW)
+        # print(l.dB)
+        l.W = l.W + l.dW * alpha
 
-            n.B = n.B - (n.dB * alpha)
-            n.W = n.W - (n.dW * alpha)
+        l.B = l.B + l.dB * alpha
 
-    return
-
-def error(A,Y):
-    tmp = 0
-    for i in range(len(Y)):
-        tmp += (Y[i]-A[i])**2
-    return tmp
 
 def forwardNN(E,Layers):
     i = 0
     # Eingabevektor in Eingabelayer schreiben
-    for e in E:
-        Layers[0].neurons[i].A = e
-        i = i + 1
+    Layers[0].A = E
+
     # Layer für Layer aus dem vorherigen layer berechnen
     for l in Layers:
         l.forward()
 
     return Layers[-1].A
 
-E1 = [0.0, 0.0]
-E2 = [1.0, 0.0]
-E3 = [0.0, 1.0]
-E4 = [1.0, 1.0]
+
+def error(A, Y):
+    tmp = 0
+    for i in range(len(Y)):
+        tmp += (Y[i]-A[i])**2
+    return tmp/len(A)
+
+E1 = np.matrix([[0.0], [0.0]])
+E2 = np.matrix([[1.0], [0.0]])
+E3 = np.matrix([[0.0], [1.0]])
+E4 = np.matrix([[1.0], [1.0]])
+Y = [np.matrix([[0.0], [0.0], [0.0]]),
+     np.matrix([[0.0], [1.0], [1.0]]),
+     np.matrix([[0.0], [1.0], [1.0]]),
+     np.matrix([[1.0], [1.0], [0.0]])]
+
 E = [E1, E2, E3, E4]
-Y = [[0.0, 0.0, 0.0],
-     [0.0, 1.0, 1.0],
-     [0.0, 1.0, 1.0],
-     [1.0, 1.0, 0.0]]
-Layers = [In, Mid, Out]
-
-#print(forwardNN(E1,Layers))
-#print(forwardNN(E2,Layers))
 
 
-for i in range(1000):
-    x = np.random.random_integers(0, 3)
-    A = forwardNN(E[x], Layers)
-    #print(E[x])
-    #print(A)
-    #print(Y[x])
-    #print()
+for i in range(3000000):
+    x = np.random.randint(0,4)
+    forwardNN(E[x], Layers)
+    backPropNN(Out.A, Layers, Y[x])
+    if (i % 10000) == 0:
 
-    if (i % 1000) == 0:
-        print(error(A[0], Y[x]))
-    backPropNN(E[x], Layers, Y[x])
-
-for x in range(4):
-    A = forwardNN(E[x], Layers)
-    print(error(A[0], Y[x]))
-    print(A)
-    print(Y[x])
-
-
-#print(forwardNN(E4,Layers))
-
-
+        print("Errors", i/1000)
+        print(error(forwardNN(E[0], Layers), Y[0])[0, 0])
+        print(error(forwardNN(E[1], Layers), Y[1])[0, 0])
+        print(error(forwardNN(E[2], Layers), Y[2])[0, 0])
+        print(error(forwardNN(E[3], Layers), Y[3])[0, 0])
 
 #
 #    E = [E1,E2,E3,E4]
